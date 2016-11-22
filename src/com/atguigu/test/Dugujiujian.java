12f2d74b470018b0b416093236ef6ce4c11bd55d
shengshadegushi <i1415594833@sina.com>
2016-11-22 20:46:17 +0800

    test pull... 的config配置文件

--- src/com/atguigu/test/Dugujiujian.java
+++ src/com/atguigu/test/Dugujiujian.java
@@ -12,7 +12,7 @@ public class Dugujiujian {
 		System.out.println("测试冲突 conflict3");
 		System.out.println("测试prepare with ");
 		System.out.println("测试 为了pull... 缺少一个界面而又分支不错乱的pull下来，"
-				+ "而修改此仓库的config配置文件");
+				+ "而修改此仓库的config配置文件 123");
 	}
 	
 }
