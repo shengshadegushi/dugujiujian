7d514e1dfebf37756ff67d71434a404870376fb6
linghuchong <i1415594833@sina.com>
2016-11-22 20:44:57 +0800

    update 05

--- src/com/atguigu/test/Dugujiujian.java
+++ src/com/atguigu/test/Dugujiujian.java
@@ -9,8 +9,10 @@ public class Dugujiujian {
 		System.out.println("conflict 02");
 		System.out.println("conflict1");
 		System.out.println("conflict2");
-		System.out.println("conflict3");
-		System.out.println("conflict3");
+		System.out.println("测试冲突 conflict3");
+		System.out.println("测试prepare with ");
+		System.out.println("测试 为了pull... 缺少一个界面而又分支不错乱的pull下来，"
+				+ "而修改此仓库的config配置文件");
 	}
 	
 }
