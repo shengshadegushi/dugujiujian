87d98aa4ee19efeadfd27a35dd148ddb8ce961b3
linghuchong <i1415594833@sina.com>
2016-11-22 20:18:04 +0800

    update 02

--- src/com/atguigu/test/Dugujiujian.java
+++ src/com/atguigu/test/Dugujiujian.java
@@ -5,6 +5,7 @@ public class Dugujiujian {
 	public static void main(String[] args) {
 		System.out.println("hello! dugujiujian");
 		System.out.println("conflict 01");
+		System.out.println("conflict 01.1");
 	}
 	
 }
